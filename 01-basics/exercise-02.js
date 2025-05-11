3a3eccf2ae0ce855394bc44d7918585096235b76
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 10:48:58 +0200

    Exercice 2 : Concaténation de chaînes de caractères

---
+++ 01-basics/exercise-02.js
@@ -0,0 +1,10 @@
+// Exercice 02 : Concaténer deux chaînes de caractères
+
+let prenom = "Vanilla";
+let ville = "Lille";
+let age = 35;
+
+let message = prenom + " habite à " + ville + ".";
+let finalMessage = message + " Ella a " + age + " ans.";
+
+console.log(finalMessage);
