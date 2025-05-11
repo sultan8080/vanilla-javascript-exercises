9c126421b30d19760b3b109d192e7c6c312a3f7b
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 10:53:35 +0200

    Exercice 3 : Addition de deux nombres

---
+++ 01-basics/exercise-03.js
@@ -0,0 +1,8 @@
+// Exercice 03 : Additionner deux nombres et afficher le résultat
+
+let x = 67;
+let y = 58;
+
+let addition = x+y;
+
+console.log ("L'addition de " + x + " et " +y +" est = " + addition + ".");
