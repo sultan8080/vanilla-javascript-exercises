57e1424f08ee62e14d5cd0a9b6b73064886655f4
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 11:00:50 +0200

    Exercice 5 : Comparaison de deux valeurs

---
+++ 01-basics/exercise-05.js
@@ -0,0 +1,12 @@
+// Exercice 05 : Comparer deux nombres
+
+let x = 12;
+let y = 12;
+
+if (x > y) {
+  console.log(x + " est plus grand que " + y);
+} else if (x < y) {
+  console.log(x + " est plus petit que " + y);
+} else {
+  console.log(x + " est égal à " + y);
+}
