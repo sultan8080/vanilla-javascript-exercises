c9c25677c4b41b7d931935bfe936d9d7033810ee
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 19:54:41 +0200

    Exercice 03 : Conversion en fonction fléchée

---
+++ 02-fonctions/exercice-03.js
@@ -0,0 +1,11 @@
+//Exercice 03 : Fonction fléchée
+// Réécris la fonction addition de l’exercice précédent sous forme de fonction fléchée.
+
+
+// Fonction fléchée - arrow funciton;
+
+const addition2 = (x, y) => {
+  return x + y;
+};
+
+console.log(addition2(50, 20));
