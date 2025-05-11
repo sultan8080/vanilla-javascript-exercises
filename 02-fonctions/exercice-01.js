6828570f1f5421e84cba93450e6ec097d42103dd
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 19:45:59 +0200

    Exercice 01 : Création d'une fonction simple qui retourne une chaîne

---
+++ 02-fonctions/exercice-01.js
@@ -0,0 +1,14 @@
+//Exercice 01 : Déclaration de fonction
+//Déclare une fonction appelée bonjour qui retourne la chaîne "Bonjour le monde !".
+
+function bonjour() {
+    return "Bonjour le monde";    
+}
+
+//Arrow function
+const bonjour1 = () => {
+    return "Bonjour le monde"; 
+};
+const bonjour2 = ()=> "bonjour le monde";
+
+console.log(bonjour());
