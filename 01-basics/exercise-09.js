2537a38d1bfd9def31e0e77e357636a92f424021
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 16:53:52 +0200

    Exercice 9 : Utilisation d'une fonction simple

---
+++ 01-basics/exercise-09.js
@@ -0,0 +1,8 @@
+// Exercice 09 : Créer une fonction qui retourne le carré d’un nombre
+
+let numero = 25;
+function carre(n) {
+  return n * n;
+}
+
+console.log("Le carré de 5 est : " + carre(numero));
