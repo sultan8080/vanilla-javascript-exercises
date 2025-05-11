bbacaf788f34b951aa450c9028fb71f1de3e29be
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 19:59:30 +0200

    Exercice 04 : Fonction anonyme assignée à une variable

---
+++ 02-fonctions/exercice-04.js
@@ -0,0 +1,7 @@
+//Crée une fonction anonyme assignée à une variable direBonjour qui retourne "Salut !".
+
+const direBonjour = function() {
+    return "Salut !";
+};
+
+console.log(direBonjour()); 
