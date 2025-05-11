be43e38079b229fa8ac970923af42c5ff439fda0
Moti-Sultan <motisultannur@gmail.com>
2025-05-11 10:59:03 +0200

    Exercice 4 : Utilisation d'une condition if/else

---
+++ 01-basics/exercise-04.js
@@ -0,0 +1,10 @@
+// Exercice 04 : Vérifier si une personne est majeure
+
+let nom = "Vanilla";
+let age = 35;
+
+if (age >= 18) {
+    console.log(nom + " est Majeure.");
+} else {
+    console.log(nom + " est Mineure.");
+}
