17c617e69a3c17028615ea546d32b9d9967bce38
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 10:51:32 +0200

    Exercice 02 : Manipulation d’un tableau avec map()

---
+++ 03-tableaux-et-objets/exercice-02.js
@@ -0,0 +1,11 @@
+// Utilise map() pour doubler chaque nombre d’un tableau
+let valeurs = [3, 6, 8, 2, 9];
+
+
+let doubleValeurs = valeurs.map(n=> {
+return n*2
+})
+
+// let doubleValeurs = valeurs.map(n=> n*2)
+
+console.log(doubleValeurs);
