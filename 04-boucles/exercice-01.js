2e2e89ae554d891e807133280844fb2a6a5c0fe6
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 13:25:31 +0200

    Exercice 01 : Boucle for pour additionner de 1 à 10

---
+++ 04-boucles/exercice-01.js
@@ -0,0 +1,10 @@
+// Exercice 01 : Boucle for - Somme des nombres de 1 à 10
+
+let somme = 0;
+
+for (let i = 1; i <= 10; i++) {
+    somme += i;
+    console.log(i + " "+somme);
+}
+
+console.log("La somme des nombres de 1 à 10 est:", somme);
