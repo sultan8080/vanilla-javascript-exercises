349d151e6f97f02b635bf1448fab01c026651c2a
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 16:11:20 +0200

    Exercice 09 : Déstructuration d’un objet

---
+++ 05-objets/exercice-09.js
@@ -0,0 +1,10 @@
+// Exercice 09 : Déstructuration
+
+const livre = {
+  titre: "Le Petit Prince",
+  auteur: "Antoine de Saint-Exupéry"
+};
+
+const { titre, auteur } = livre;
+
+console.log(titre, auteur);
