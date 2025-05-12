45b8815c6d81468824c4fd75945a63a70e9bfaaa
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 12:21:48 +0200

    Exercice 10 : Tri d’un tableau de nombres

--- 03-tableaux-et-objets/exercice-10.js
+++ 03-tableaux-et-objets/exercice-10.js
@@ -7,7 +7,7 @@ let decroissantNombre = nombres.slice().sort((a,b) => b-a);
 
 // The .slice() method in JavaScript creates a copy of part of an array without modifying the original array.
 //The ...nombres syntax is called the spread operator (...). It is used to expand the elements of an array into a new structure.
-// ...nombres, comy the orignal array nombres
+// ...nombres, copy the orignal array nombres
 
 let croissantNombre1 = [...nombres].sort((a,b) => a-b);
 let decroissantNombre1 = [...nombres].sort((a,b) => b-a);
