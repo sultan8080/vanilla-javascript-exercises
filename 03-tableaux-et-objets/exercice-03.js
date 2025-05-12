caa584f4e3a9764f390e54e64d1673a6b55e2ae9
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 10:54:02 +0200

    Exercice 03 : Filtrage d’un tableau avec filter()

---
+++ 03-tableaux-et-objets/exercice-03.js
@@ -0,0 +1,7 @@
+// Exercice 03 : Utilisation de filter()
+// Utilise filter() pour ne garder que les nombres supérieurs à 5.
+
+let valeurs = [3, 6, 8, 2, 9];
+
+let supérieursValue = valeurs.filter(n => n > 5);
+console.log(supérieursValue);
