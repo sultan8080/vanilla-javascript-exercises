163787a759ce00aba43f4b07caf69f68b9be626d
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 10:27:27 +0200

    Exercice 10 : IIFE pour exécuter une fonction immédiatement

---
+++ 02-fonctions/exercice-10.js
@@ -0,0 +1,6 @@
+//Exercice 10 : IIFE (fonction immédiatement invoquée)
+// Écris une fonction IIFE qui affiche "Exécuté immédiatement" dans la console.
+
+(function () {
+  console.log("Exécuté immédiatement");
+})();
