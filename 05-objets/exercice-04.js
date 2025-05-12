d6ef22e15a5787ba43a1d65524b06e3cbd896580
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 16:04:52 +0200

    Exercice 04 : Copier des propriétés avec Object.assign

---
+++ 05-objets/exercice-04.js
@@ -0,0 +1,9 @@
+// Exercice 04 : Copier des propriétés avec Object.assign
+// Exercice 04 : Copier des propriétés avec Object.assign
+
+const cible = { a: 1 };
+const source = { b: 2, c: 3 };
+
+Object.assign(cible, source);
+
+console.log(cible); 
