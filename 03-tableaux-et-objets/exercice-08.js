522eb7291cfe9cca026eb5b29b611caf139cb322
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 11:50:45 +0200

    Exercice 08 : Déstructuration d’objet

---
+++ 03-tableaux-et-objets/exercice-08.js
@@ -0,0 +1,12 @@
+// Utilise la déstructuration pour extraire des propriétés d’un objet.
+let voiture = {
+  marque: "Toyota",
+  modele: "Corolla",
+  annee: 2020
+};
+
+let {marque, modele, annee} = voiture;
+
+console.log(marque);
+console.log(modele);
+console.log(annee);
