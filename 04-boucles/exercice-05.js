44b067d4fe10a35a258409b59d9964f60b40d7a2
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 14:29:37 +0200

    Exercice 05 : Boucles imbriquées pour une table de multiplication

---
+++ 04-boucles/exercice-05.js
@@ -0,0 +1,8 @@
+// Exercice 05 : Boucles imbriquées - Table de multiplication
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Table de ${i} :`);
+  for (let j = 0; j <= 10; j++) {
+    console.log(`${i} X ${j} = ${i * j}`);
+  }
+}
