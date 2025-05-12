f1620d2a5c41129cac66cc64d2425b1129fb0f05
Moti-Sultan <motisultannur@gmail.com>
2025-05-12 20:37:43 +0200

    Exercice 07 : Modifier la promesse avec nouvel exemple

--- 06-ES6-Pratiques/exercice-07.js
+++ 06-ES6-Pratiques/exercice-07.js
@@ -1,10 +1,16 @@
 // Exercice 07 : Promesse qui se résout après un délai
-// Exercice 07 : Promesse qui se résout après un délai
 
-const promesse = new Promise((resolve) => {
+const promesse = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Promesse tenue !");
+    let success = Math.random() > 0.5;
+    if (success) {
+      resolve("Promesse tenue !");
+    } else {
+      reject("Promesse non tenue !");
+    }
   }, 1000);
 });
 
-promesse.then((message) => console.log(message));
+promesse
+  .then((message) => console.log(message))
+  .catch((error) => console.log( error));
