daaf9f40b192837aff23bdb17c4a9c773456615b
Moti-Sultan <motisultannur@gmail.com>
2025-05-13 20:31:46 +0200

    Exercice 01 : Sélection d’un élément par ID

---
+++ 10-dom-events/exercice-01.js
@@ -0,0 +1,5 @@
+// Exercice 01 : Sélectionner un élément par son ID et changer son contenu
+
+function changerTexte() {
+  document.getElementById("monElement").textContent = "Texte modifié, Bonjour Dom !";
+}
